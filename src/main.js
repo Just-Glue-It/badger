b2cc4eab9a522fafb8c6172f3873b24d8bd3a2c6
Scott King <kingsc850@gmail.com>
2015-11-12 19:44:13 -0500

    fixed webpack minus import statements :cry:

--- src/main.js
+++ src/main.js
@@ -1,29 +1,27 @@
-// const Cycle = require('@cycle/core');
-// const CycleDOM = require('@cycle/dom');
-
-import webpack from 'webpack';
+const Cycle = require('@cycle/core');
+const CycleDOM = require('@cycle/dom');
+const h = CycleDOM.h;
+const makeDOMDriver = CycleDOM.makeDOMDriver;
 
 // import Cycle from '@cycle/core';
 // import {makeDOMDriver, h} from '@cycle/dom';
 
-// function main(responses) {
-//   const requests = {
-//     DOM: responses.DOM.select('.field').events('input')
-//       .map(ev => ev.target.value)
-//       .startWith('')
-//       .map(name =>
-//         CycleDOM.h('div', [
-//           CycleDOM.h('label', 'Name:'),
-//           CycleDOM.h('input.field', {attributes: {type: 'text'}}),
-//           CycleDOM.h('h1', 'Hello ' + name)
-//         ])
-//       )
-//   };
-//   return requests;
-// }
-//
-// Cycle.run(main, {
-//   DOM: CycleDOM.makeDOMDriver('#app')
-// });
+function main(responses) {
+  const requests = {
+    DOM: responses.DOM.select('.field').events('input')
+      .map(ev => ev.target.value)
+      .startWith('')
+      .map(name =>
+        h('div', [
+          h('label', 'Name:'),
+          h('input.field', {attributes: {type: 'text'}}),
+          h('h1', 'Hello ' + name)
+        ])
+      )
+  };
+  return requests;
+}
 
-console.log('hello, world');
+Cycle.run(main, {
+  DOM: makeDOMDriver('#app')
+});

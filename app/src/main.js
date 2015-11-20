d4bd22e834c39990ff96bb0ce8a9b576ffdcb8b0
Jordan Schroter <jordan.schroter@gmail.com>
2015-11-19 19:17:19 -0500

    working

--- app/src/main.js
+++ app/src/main.js
@@ -1,22 +1,27 @@
-import Cycle from '@cycle/core';
-import {makeDOMDriver, h} from '@cycle/dom';
-
-function main(responses) {
-  const requests = {
-    DOM: responses.DOM.select('.field').events('input')
-      .map(ev => ev.target.value)
-      .startWith('')
-      .map(name =>
-        h('div', [
-          h('label', 'Stuff:'),
-          h('input.field', {attributes: {type: 'text'}}),
-          h('h1', 'Hello ' + name)
-        ])
-      )
-  };
-  return requests;
-}
-
-Cycle.run(main, {
-  DOM: makeDOMDriver('#app')
-});
+import Rx from 'rx';
+// const Rx = require('rx');
+console.log(Rx);
+//
+// import Cycle from '@cycle/core';
+// import {makeDOMDriver, h} from '@cycle/dom';
+//
+//
+// function main(responses) {
+//   const requests = {
+//     DOM: responses.DOM.select('.field').events('input')
+//       .map(ev => ev.target.value)
+//       .startWith('')
+//       .map(name =>
+//         h('div', [
+//           h('label', 'Stuff:'),
+//           h('input.field', {attributes: {type: 'text'}}),
+//           h('h1', 'Hello ' + name)
+//         ])
+//       )
+//   };
+//   return requests;
+// }
+//
+// Cycle.run(main, {
+//   DOM: makeDOMDriver('#app')
+// });

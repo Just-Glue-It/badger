ab4a5f67e1c576bf09a910235cd90e145b4482de
Scott King <kingsc850@gmail.com>
2015-11-23 11:52:00 -0500

    added some minor changes to main.js to prepare for dev

--- app/src/main.js
+++ app/src/main.js
@@ -1,22 +1,12 @@
 import Cycle from '@cycle/core';
-import {makeDOMDriver, h} from '@cycle/dom';
+import {makeDOMDriver} from'@cycle/dom';
 
-function main(responses) {
-  const requests = {
-    DOM: responses.DOM.select('.field').events('input')
-      .map(ev => ev.target.value)
-      .startWith('')
-      .map(name =>
-        h('div', [
-          h('label', 'Stuff:'),
-          h('input.field', {attributes: {type: 'text'}}),
-          h('h1', 'Hello ' + name)
-        ])
-      )
-  };
-  return requests;
-}
+// TODO: create components and drivers
+// import components from ...
+// import drivers from ...
 
-Cycle.run(main, {
-  DOM: makeDOMDriver('#app')
+// const main = components;
+
+run(main, {
+  // DOM: ...
 });

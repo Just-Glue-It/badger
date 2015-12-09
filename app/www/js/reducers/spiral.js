d305d6f048c9289b81ccb75619f4129cf2540b89
David Rusu <davidrusu.me@gmail.com>
2015-12-09 03:56:45 -0500

    local storage for pings works

--- app/www/js/reducers/spiral.js
+++ app/www/js/reducers/spiral.js
@@ -3,8 +3,21 @@ import {ADD_SPIRAL_DATA_ACTION} from './../constants/action-constants';
 import AppContainer from '../containers/AppContainer.js';
 import HomeContainer from '../containers/HomeContainer.js';
 
+function loadPings() {
+  const local = localStorage.getItem('pings');
+  if (local) {
+    return List(JSON.parse(local));
+  } else {
+    return List();
+  }
+}
+
+function writePings(pings) {
+  localStorage.setItem('pings', JSON.stringify(pings.toJS()));
+}
+
 const initialState = new Map({
-  pings: List()
+  pings: loadPings()
 });
 
 export default function reducer(state = initialState, action = {}) {
@@ -16,7 +29,11 @@ export default function reducer(state = initialState, action = {}) {
       time: action.ping.time,
       color: Math.random() * 255
     };
-    return state.update('pings', pings => pings.push(ping));
+    return state.update('pings', pings => {
+      let newPings = pings.push(ping);
+      writePings(newPings);
+      return newPings;
+    });
   default:
     return state;
   }

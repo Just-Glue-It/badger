135f35b664b3e20472cbb4e47d0506073c0dc302
David Rusu <davidrusu.me@gmail.com>
2016-02-13 17:04:14 -0500

    removed login and register

--- app/www/js/reducers/reducer.js
+++ app/www/js/reducers/reducer.js
@@ -1,16 +1,12 @@
 import {combineReducers} from 'redux';
 import dummy from './dummy';
 import route from './route';
-import login from './login';
 import home from './home';
-import register from './register';
 import spiral from './spiral';
 
 export default combineReducers({
   route,
   dummy,
-  login,
   home,
-  register,
   spiral
 });
